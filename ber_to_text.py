7af2aad8269e1f4ec725b86b3afe3868dcd9b286
thierrydutoit <31282663+thierrydutoit@users.noreply.github.com>
2025-04-07 17:05:38 +0200

    Update ber_to_text.py

--- ber_to_text.py
+++ ber_to_text.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 
 # --- Utility functions ---
 def text_to_bits(text):
@@ -20,23 +21,44 @@ def bits_to_text(bits):
     text = ''.join([chr(int(char, 2)) for char in chars])
     return text
 
-# --- Streamlit Interface ---
 st.title("Binary Transmission Error Simulator")
+st.markdown('''What is the actual effect of a given bit error rate on text? \\
+               In this application, text is encoded as 7-bit ASCII characters and 
+               the resulting bit sequence is artificially scrambled with a given bit 
+               error rate (BER).''')
 
-# Step 1: Text input
-message = st.text_area("Enter your message:", "Audio watermarking: this message will be embedded in an audio signal, using spread spectrum modulation, and later retrieved from the modulated signal.")
+col1, col2 = st.columns(2)
 
-if message:
-    bits = text_to_bits(message)
-    st.write(f"Number of bits: {len(bits)}")
-    st.text(f"Original bits (preview): {bits[:50]} ...")
+ber = st.slider("Bit Error Rate (BER)", 0.0, 0.1, 0.001, 0.001, format="%0.3f")
 
-    # Step 2: BER Slider
-    ber = st.slider("Bit Error Rate (BER)", 0.0, 0.1, 0.001, 0.001, format="%0.3f")
-    bits_with_errors = introduce_errors(bits, ber)
-    st.text(f"Modified bits (preview): {bits_with_errors[:50]} ...")
+with col1:
+   st.subheader("Emitted message:")
+   message = st.text_area("Enter your message:", "Binary Transmission Error Simulator")
+   bits = text_to_bits(message)     
+   
 
-    # Step 3: Reconstruction
-    reconstructed_message = bits_to_text(bits_with_errors)
-    st.subheader("Reconstructed message:")
-    st.text(reconstructed_message)
+with col2:
+   st.subheader("Received message:")
+   st.write(f"Number of bits: {len(bits)}")
+       
+   bits_with_errors = introduce_errors(bits, ber)
+   reconstructed_message = bits_to_text(bits_with_errors)
+      
+   st.text(reconstructed_message)
+
+   fig,ax = plt.subplots(figsize=(10,4))
+
+   plt.xlim(1, len(bits))
+   plt.ylim(-0.1, 1.1)    
+   ax.plot(np.arange(len(bits)),bits, label="Emitted bits")
+   ax.plot(np.arange(len(bits)),bits_with_errors, label="Received bits")
+   ax.set_xlabel("Index")
+   ax.set_ylabel("Value")
+   ax.legend()
+   st.pyplot(fig)
+
+with st.expander("Open for comments"):
+   st.markdown('''Obviously, a BER of 0.01 or lower makes transmision errors acceptable
+                  to the eye. \\
+                  Notice that, in practice, error correction codes can be added to 
+                  encoded bits, to detect and correct such transmission errors.''')
